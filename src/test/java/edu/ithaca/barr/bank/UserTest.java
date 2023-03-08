ee4c9475cb7047470d4104617c79b2a5f1629fac
maxbraith <mbraithwaite@ithaca.edu>
2023-03-08 00:10:39 -0500

    again

---
+++ src/test/java/edu/ithaca/barr/bank/UserTest.java
@@ -0,0 +1,12 @@
+package edu.ithaca.barr.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UserTest{
+    @Test
+    void checkOutBookTest(){
+
+    }
+}
